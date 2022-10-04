cd901266da4da081a26bfc1570ff4b7aac599a6a
Veninagarapu <93289791+Veninagarapu@users.noreply.github.com>
2022-10-04 15:05:51 +0100

    Update tflapp.js

--- src/components/tflapp.js
+++ src/components/tflapp.js
@@ -18,7 +18,7 @@ function TFLProject() {
 API and allows users to plan their journey and to know the live locations of
 the stations using google maps.Added a visual depiction of the user's journey, When the user has selected
 a journey, display their journey with a line from one station to the next in
-the color of that train line. Used React hooks and React router to navigate to different pages. Utilized Bootstrap and Figma to have a user-friendly front end.`}</p>
+the color of that train line. Used React hooks and React router to navigate to different pages.Utilized Bootstrap and Figma to have a user-friendly front end.`}</p>
         <div className="link-buttons-container">
           <button className="portfolio-link"><a href="https://storied-rabanadas-de7a71.netlify.app/" rel="noreferrer" target="_blank">Live site</a></button>
           <button className="portfolio-link"><a href=" https://github.com/Veninagarapu/Transport-for-London-App" rel="noreferrer" target="_blank">Github Repository</a></button>
